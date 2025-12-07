a643a3a4f391dca17658c5b4fb37140e80f32c1b
Raman Verma <ramanverma4183@gmail.com>
2025-12-07 10:40:28 +0530

    feat(RetryMiddleware): API changes in `whenError` and `whenResponse`

--- lib/src/middlewares/retry.dart
+++ lib/src/middlewares/retry.dart
@@ -8,7 +8,7 @@ import '../utils/request_copier.dart';
 abstract interface class BackoffStrategy {
   const BackoffStrategy();
 
-  Duration getDelay(int attempt);
+  Duration getDelayDuration(int attempt);
 }
 
 /// A strategy that waits for a fixed duration.
@@ -17,7 +17,7 @@ class FixedDelayStrategy implements BackoffStrategy {
   final Duration delay;
 
   @override
-  Duration getDelay(int attempt) => delay;
+  Duration getDelayDuration(int attempt) => delay;
 }
 
 /// A strategy that waits for a duration that increases linearly with the attempt count.
@@ -26,7 +26,7 @@ class LinearBackoffStrategy implements BackoffStrategy {
   final Duration initialDelay;
 
   @override
-  Duration getDelay(int attempt) => initialDelay * attempt;
+  Duration getDelayDuration(int attempt) => initialDelay * attempt;
 }
 
 /// A strategy that waits for a duration that increases exponentially with the attempt count.
@@ -37,7 +37,7 @@ class ExponentialBackoffStrategy implements BackoffStrategy {
   final Duration initialDelay;
 
   @override
-  Duration getDelay(int attempt) {
+  Duration getDelayDuration(int attempt) {
     return Duration(
       milliseconds: initialDelay.inMilliseconds * pow(2, attempt - 1).toInt(),
     );
@@ -54,14 +54,23 @@ class RetryMiddleware implements Middleware {
   });
   final int maxRetries;
   final BackoffStrategy strategy;
-  final bool Function(Object error)? whenError;
-  final bool Function(BaseResponse response)? whenResponse;
+  final bool Function(Object error, int attempt, Duration nextAttempt)?
+  whenError;
+  final bool Function(
+    BaseResponse response,
+    int attempt,
+    Duration totalDuration,
+  )?
+  whenResponse;
 
   @override
   Future<StreamedResponse> handle(BaseRequest request, Handler next) async {
     var attempts = 0;
+
     while (true) {
       attempts++;
+
+      final duration = strategy.getDelayDuration(attempts);
 
       final currentRequest = (attempts == 1) ? request : copyRequest(request);
 
@@ -69,26 +78,37 @@ class RetryMiddleware implements Middleware {
         final response = await next(currentRequest);
         if (attempts <= maxRetries &&
             whenResponse != null &&
-            whenResponse!(response)) {
-          await response.stream.drain<void>();
-          await _delay(attempts);
+            whenResponse!(
+              response,
+              attempts,
+              duration,
+            )) {
+          await Future.wait([
+            response.stream.drain<void>(),
+            _delayBy(duration),
+          ]);
+
           continue;
         }
         return response;
       } catch (e) {
-        if (attempts > maxRetries || (whenError != null && !whenError!(e))) {
+        final delayDuration = strategy.getDelayDuration(attempts);
+
+        if (attempts > maxRetries ||
+            (whenError != null && !whenError!(e, attempts, delayDuration))) {
           if (whenError == null && attempts <= maxRetries) {
-            await _delay(attempts);
+            await _delayBy(delayDuration);
             continue;
           }
           rethrow;
         }
-        await _delay(attempts);
+
+        await _delayBy(delayDuration);
       }
     }
   }
 
-  Future<void> _delay(int attempt) {
-    return Future.delayed(strategy.getDelay(attempt));
+  Future<void> _delayBy(Duration duration) {
+    return Future.delayed(duration);
   }
 }
